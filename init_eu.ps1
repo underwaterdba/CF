b4e3784d2c74159581f9d136cb03732568518ae4
underwaterdba <47184943+underwaterdba@users.noreply.github.com>
2019-05-12 15:52:40 +0100

    Update init_eu.ps1

--- init_eu.ps1
+++ init_eu.ps1
@@ -1,7 +1,7 @@
 Set-DnsClientServerAddress -InterfaceIndex 12 -ServerAddresses ("10.79.2.10","10.79.2.11")
 
 
-$Secure_String_Pwd = ConvertTo-SecureString "@t$5%L3WVa%" -AsPlainText -Force
+$Secure_String_Pwd = ConvertTo-SecureString "@t#5%L3WVa%" -AsPlainText -Force
 
 $mycreds = New-Object System.Management.Automation.PSCredential ("alacra.eu\administrator", $Secure_String_Pwd)
 
