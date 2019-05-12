70c20cb98a8b3fd60b72ef957140ad45af5d7d32
underwaterdba <47184943+underwaterdba@users.noreply.github.com>
2019-05-12 13:32:25 +0100

    Update Init.ps1

--- Init.ps1
+++ Init.ps1
@@ -4,3 +4,4 @@ $d = get-date
 if ((test-path "c:\serverlog") -eq $false) {mkdir "c:\serverlog"}
 $d.tostring("yyyy-MM-dd hh:mm:ss") | out-file "c:\serverlog\log.txt"
 
+netsh.exe advfirewall set allprofiles state off
