7b380c631a3ddfacfb4b16d559989a23b302e64d
underwaterdba <47184943+underwaterdba@users.noreply.github.com>
2020-01-27 15:27:29 +0000

    Update Init.ps1

--- Init.ps1
+++ Init.ps1
@@ -1,3 +1,5 @@
+Param ($NewName)
+
 $d = get-date
 
 
@@ -5,3 +7,10 @@ if ((test-path "c:\serverlog") -eq $false) {mkdir "c:\serverlog"}
 $d.tostring("yyyy-MM-dd hh:mm:ss") | out-file "c:\serverlog\log.txt"
 
 netsh.exe advfirewall set allprofiles state off
+
+#Set-DnsClientServerAddress -InterfaceIndex 12 -ServerAddresses ("10.79.2.10","10.79.2.11")
+
+#Add-Computer -DomainName MYLAB.Local -ComputerName TARGETCOMPUTER -newname NewTARGETCOMPUTER
+Rename-Computer -NewName "Server044" 
+
+restart-computer -force
