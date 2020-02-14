b56f87b26cfedd029c9ab3e665dddabe345e4553
underwaterdba <47184943+underwaterdba@users.noreply.github.com>
2020-02-14 19:46:08 +0000

    Update Init.ps1

--- Init.ps1
+++ Init.ps1
@@ -8,7 +8,7 @@ $d.tostring("yyyy-MM-dd hh:mm:ss") | out-file "c:\serverlog\log.txt"
 
 netsh.exe advfirewall set allprofiles state off
 
-Set-DnsClientServerAddress -InterfaceIndex 12 -ServerAddresses ("10.24.45.33","10.24.46.33","10.30.9.33","10.30.10.33")
+Set-DnsClientServerAddress -InterfaceIndex 12 -ServerAddresses ("10.24.45.34","10.24.46.34","10.30.9.33","10.30.10.33")
 
 #Add-Computer -DomainName MYLAB.Local -ComputerName TARGETCOMPUTER -newname NewTARGETCOMPUTER
 Rename-Computer -NewName $NewName -force -restart
